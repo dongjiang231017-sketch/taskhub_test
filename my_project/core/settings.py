d4c38ea10bc469cddff8beae9da65b5eb0bc46a8
adong <347368694@qq.com>
2026-04-27 15:56:43 +0800

    Fix SimpleUI hash tab routing

--- my_project/core/settings.py
+++ my_project/core/settings.py
@@ -197,7 +197,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
